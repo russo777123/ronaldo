ed5342c458f9dde2ad43d01e7b8eaf50084015e3
Yuriandrews21 <yurilojavirtual@gmail.com>
2026-01-28 19:27:56 -0300

    2.0

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import Database from 'better-sqlite3'
-
 import path from 'path'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 const dbPath = path.join(process.cwd(), 'prisma', 'dev.db')
-const db = new Database(dbPath)
-const adapter = new PrismaBetterSqlite3(db)
+const adapter = new PrismaBetterSqlite3({
+  url: `file:${dbPath}`
+})
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
+    log: ['error', 'warn'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
